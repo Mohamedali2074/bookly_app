3f7a1ede4b25650baa8726a3688edb0bdfe220da
Mohamedali2074 <mohammedali.w74@gmail.com>
2023-02-12 16:50:06 +0200

    best seller listview item part 1

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -25,6 +25,40 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.tittleMedium,
           ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.6 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsData.testImage,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          ),
         ],
       ),
     );
