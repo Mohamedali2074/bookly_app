e9118f84095c7a7a8577552399409b79b405e17f
Mohamedali2074 <mohammedali.w74@gmail.com>
2023-02-12 11:20:12 +0200

    add custom appbar to home view

---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+
+
+import 'custom_app_bar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        CoustomAppBar(),
+      ],
+    );
+  }
+}
+
+
